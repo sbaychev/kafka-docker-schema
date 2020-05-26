169e6a8930ce8c6be82576475698dc47369f8794
Stefan Baychev <sbaychev@gmail.com>
2020-05-26 22:15:55 +0300

    bad files

--- bin/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# This script builds the final artefact that will be packaged in the Docker image. It is run automatically run by
-# Bitbucket (see `bitbucket-pipeline.yml`).
-# The AWS variables must exist as environment variables in Bitbucket.
-
-./gradlew --build-cache bootJar -PAWS_ACCESS_KEY=$AWS_ACCESS_KEY -PAWS_SECRET_KEY=$AWS_SECRET_KEY
--- bin/integration-tests.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# This script runs the integration tests in Bitbucket pipelines (see `bitbucket-pipeline.yml`).
-
-# The AWS variables must exist as environment variables in Bitbucket.
-./gradlew --build-cache integrationTest -PAWS_ACCESS_KEY=$AWS_ACCESS_KEY -PAWS_SECRET_KEY=$AWS_SECRET_KEY
